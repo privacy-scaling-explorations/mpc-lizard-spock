b83d0e83050120485310544a2df9770d1de90c81
cedoor <me@cedoor.dev>
2025-01-20 17:13:33 +0000

    feat: add progress bar for mpc computation

--- src/Calculating.tsx
+++ src/Calculating.tsx
@@ -1,5 +1,24 @@
+import Ctx from './Ctx';
+import ProgressBar from './ProgressBar';
+
 export default function Calculating() {
+  const ctx = Ctx.use();
+  const mpcProgress = ctx.mpcProgress.use();
+
+  // Small function to adjust the computation progress.
+  // The first 20% of progress is amplified to take significantly more time,
+  // while the remaining 80% is compressed to progress faster.
+  function getEasedProgress(progress: number) {
+    const t = Math.min(1, Math.max(0, progress / 100));
+
+    if (t < 0.2) {
+      return t * 10 * 100;
+    }
+
+    return (1 + (t - 0.2) * 0.1) * 100;
+  }
+
   return <div>
-    <center>Calculating...</center>
+    <ProgressBar progress={getEasedProgress(mpcProgress)} />
   </div>;
 }
---
+++ src/ProgressBar.tsx
@@ -0,0 +1,9 @@
+import './ProgressBar.css';
+
+export default function ProgressBar({ progress }: { progress: number }) {
+  return (
+    <div className='progress-bar'>
+      <div style={{ width: `${progress}%` }}></div>
+    </div>
+  );
+}
