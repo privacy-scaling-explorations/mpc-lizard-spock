ca3b4cfc6460d128180da4aef5472a7db799fa5e
Andrew Morris <voltrevo@gmail.com>
2025-01-21 11:27:14 +1100

    Replace getEasedProgress with x^2

--- src/Calculating.tsx
+++ src/Calculating.tsx
@@ -5,18 +5,7 @@ export default function Calculating() {
   const ctx = Ctx.use();
   const mpcProgress = ctx.mpcProgress.use();
 
-  // Small function to adjust the computation progress.
-  // The first 20% of progress is amplified to take significantly more time,
-  // while the remaining 80% is compressed to progress faster.
-  function getEasedProgress(progress: number) {
-    if (progress < 0.2) {
-      return progress * 10;
-    }
-
-    return (1 + (progress - 0.2) * 0.1);
-  }
-
   return <div>
-    <ProgressBar progress={getEasedProgress(mpcProgress)} />
+    <ProgressBar progress={mpcProgress ** 2} />
   </div>;
 }
